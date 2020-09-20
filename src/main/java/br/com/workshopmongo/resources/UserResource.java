3ded36e399bfeb5b1c15176dedef3858b675898a
Edson Junior <edsonjos61@gmail.com>
2020-09-20 19:22:31 -0300

    Foi criado os pacotes domain e resources.

---
+++ src/main/java/br/com/workshopmongo/resources/UserResource.java
@@ -0,0 +1,27 @@
+package br.com.workshopmongo.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.workshopmongo.domain.User;
+
+@RestController
+@RequestMapping(value = "/")
+public class UserResource {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = new ArrayList<>();
+		User maria = new User("1" , "Maria Silva" , "maria@gmail.com");
+		User alex = new User("2" , "Alex Silva" , "alex@gmail.com");
+		list.addAll(Arrays.asList(maria, alex));
+		return ResponseEntity.ok().body(list);
+	}
+
+}
